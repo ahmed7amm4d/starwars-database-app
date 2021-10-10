c6a92d1d0b9253d8deb4244688092d58de6d14ae
Ahmed Hammad <ahmed7amm4d@gmail.com>
2021-10-11 00:15:52 +0200

    'fix-logo

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,10 +4,13 @@ import classes from './Header.module.css';
 
 const Header = (props) => {
     return (
-        <header className={classes.header}>
-            <h1>Star<br />World</h1>
+        <React.Fragment>
+            <header className={classes.header}>
+                {/* <h1>Star<br />World</h1> */}
+                <img src="https://fontmeme.com/permalink/211010/ff434dec13d3e6d9979105b386a88199.png" alt="Star World Logo" />
+            </header>
             <hr className={classes.hr1} />
-        </header>
+        </React.Fragment>
     );
 };
 
